a2e72431b2fc83844bb24595f8df103016567425
Bhallamudi Sai Narasimha Abhiram <68442741+abhirambsn@users.noreply.github.com>
2024-02-04 18:55:16 +0530

    Version updated

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ if os.getenv("ENV", "dev") == "dev":
 app = FastAPI(
     title="Bnb Clone Auth Service",
     description="This is the authentication service for the Bnb Clone Application",
-    version="0.1.0",
+    version="0.1.1",
     docs_url="/docs",
     redoc_url="/redoc",
     openapi_url="/openapi.json",
